c9d8c13aa6cf0584bfd0d20a681a8d844e454cba
ifrit98 <ifrit98@gmail.com>
2024-05-22 15:09:36 +0000

    indent

--- webdev/test_redis.py
+++ webdev/test_redis.py
@@ -1,49 +1,48 @@
 import os
+import pytest
+from passlib.context import CryptContext
+from database import get_database, startup, UserInDB, create_user, get_user
 
- import pytest
- from passlib.context import CryptContext
- from database import get_database, startup, UserInDB, create_user, get_user
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
- pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+UNUSED_DB = 15  # WARNING: This will clear the entire Redis database. Use with caution.
+PORT = 6379
+HOST = "localhost"
 
- UNUSED_DB = 15  # WARNING: This will clear the entire Redis database. Use with caution.
- PORT = 6379
- HOST = "localhost"
-
- os.environ["REDIS_HOST"] = HOST
- os.environ["REDIS_PORT"] = str(PORT)
- os.environ["REDIS_DB"] = str(UNUSED_DB)
-
-
- @pytest.fixture(scope="session", autouse=True)
- def redis_db():
-     startup()
-     redis_db = get_database()
-     redis_db.flushdb()
-     yield redis_db
-     redis_db.flushdb()
+os.environ["REDIS_HOST"] = HOST
+os.environ["REDIS_PORT"] = str(PORT)
+os.environ["REDIS_DB"] = str(UNUSED_DB)
 
 
- def test_user_creation_and_retrieval():
-     fake_user = UserInDB(
-         username="janedoe",
-         hashed_password=pwd_context.hash("password123"),
-         seed="b6825ec6168f72e90b1244b1d2307433ad8394ad65b7ef4af10966bc103a39bf",
-         wallet_name="janedoe",
-         wallet_hotkey="default",
-         wallet_mnemonic="ocean bean until sauce near place labor admit dismiss long asthma tunnel",
-     )
-     create_user(fake_user)
+@pytest.fixture(scope="session", autouse=True)
+def redis_db():
+    startup()
+    redis_db = get_database()
+    redis_db.flushdb()
+    yield redis_db
+    redis_db.flushdb()
 
-     # Retrieve the user
-     retrieved_user = get_user("janedoe")
 
-     # Assert equality
-     assert retrieved_user.dict() == {
-         "username": "janedoe",
-         "hashed_password": fake_user.hashed_password,
-         "seed": fake_user.seed,
-         "wallet_name": fake_user.wallet_name,
-         "wallet_hotkey": fake_user.wallet_hotkey,
-         "wallet_mnemonic": fake_user.wallet_mnemonic,
-     }
+def test_user_creation_and_retrieval():
+    fake_user = UserInDB(
+        username="janedoe",
+        hashed_password=pwd_context.hash("password123"),
+        seed="b6825ec6168f72e90b1244b1d2307433ad8394ad65b7ef4af10966bc103a39bf",
+        wallet_name="janedoe",
+        wallet_hotkey="default",
+        wallet_mnemonic="ocean bean until sauce near place labor admit dismiss long asthma tunnel",
+    )
+    create_user(fake_user)
+
+    # Retrieve the user
+    retrieved_user = get_user("janedoe")
+
+    # Assert equality
+    assert retrieved_user.dict() == {
+        "username": "janedoe",
+        "hashed_password": fake_user.hashed_password,
+        "seed": fake_user.seed,
+        "wallet_name": fake_user.wallet_name,
+        "wallet_hotkey": fake_user.wallet_hotkey,
+        "wallet_mnemonic": fake_user.wallet_mnemonic,
+    }
